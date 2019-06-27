2b97c72dc84bd45cdb974e15f0dd18340deff2ed
Claire Cassidy <clacassidy@gmail.com>
2019-06-26 17:45:42 -0700

    got test to pass

--- tests/public.test.js
+++ tests/public.test.js
@@ -6,8 +6,15 @@ describe('application routes', () => {
     return request(app)
       .get('/')
       .then(res => {
-        expect(rest.text).toEqual(expect.stringContaining('YOU ARE A BADASS AT CODE'));
+        expect(res.text).toEqual(expect.stringContaining('YOU ARE A BADASS AT CODE'));
       });
   });
-
+  // it('returns 404 if we hit a bad path', () => {
+  //   return request(app)
+  //     .get('/badPath')
+  //     .then(res => {
+  //       expect(res.status).toEqual(404);
+  //       expect(res.test).toEqual(expect.stringContaining('Sorry! Borked!'));
+  //     });
+  // });
 });
